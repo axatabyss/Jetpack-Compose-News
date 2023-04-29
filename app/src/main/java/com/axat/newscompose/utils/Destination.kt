d8aa2f60f02617027a1cf3aecdf4d3329c095db5
Akshat Bhuhagal <akshatbhuhagal@gmail.com>
2023-04-29 18:38:07 +0530

    Optimization of App

--- app/src/main/java/com/axat/newscompose/utils/Destination.kt
+++
@@ -1,8 +0,0 @@
-package com.axat.newscompose.utils
-
-sealed class Destination(val route: String) {
-
-    object HomeScreen : Destination("HomeScreen")
-    object NewsScreen : Destination("NewsScreen")
-
-}
